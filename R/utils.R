67e554b67d949b5b1d860368369067539f107f46
Michael Sumner <mdsumner@gmail.com>
2017-12-13 21:43:55 +1100

    fix nrows/ncols

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,12 @@
 #' Title
 #'
 #' @param xy coordinates
-#' @param ncol number of columns
-#' @param nrow number of rows
+#' @param ncols number of columns
+#' @param nrows number of rows
 #' @param prj projection metadata
 #'
 #' @return raster
 #' @export
-defaultgrid <- function(xy, ncol = 60, nrow = 50, prj = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0") {
-  raster::raster(raster::extent(xy), ncol = 60, nrow = 50, crs = prj)
+defaultgrid <- function(xy, ncols = 60, nrows = 50, prj = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0") {
+  raster::raster(raster::extent(xy), ncols = ncols, nrows = ncols, crs = prj)
 }
