43502f27472680f6809437d0c92e3583efc26fcd
Michael Sumner <mdsumner@gmail.com>
2019-02-25 04:30:08 +0000

    require updated geometry

--- tests/testthat/test-tri_fun.R
+++ tests/testthat/test-tri_fun.R
@@ -9,7 +9,7 @@ xy <- raster::sampleRandom(r, size = 150, xy = TRUE)[, 1:2, drop = FALSE]
 test_that("tri_fun works", {
   tri_est <- tri_fun(xy, raster::extract(r, xy)) %>% expect_s4_class("RasterLayer")
   expect_equal(dim(tri_est), c(50L, 60L, 1L))
-  expect_equivalent(sum(values(tri_est), na.rm = TRUE), 364629.086999875)
+  expect_equivalent(sum(values(tri_est), na.rm = TRUE), 360501.3478)
 })
 
 test_that("tri_pip works", {
@@ -17,5 +17,5 @@ test_that("tri_pip works", {
   set.seed(1)
   p <- jitter(xy[c(1, 10, 20, 40, 40, 50, 60, 70, 80), ], 20)
   pp <- tri_pip(list(T = tri, P = xy), sp::SpatialPoints(p))
-  expect_equal(pp, c(180L, 191L, 82L, 36L, 10L, 159L, 226L, 277L, 142L))
+  expect_equal(pp, c(208L, 157L, 252L, 261L, 3L, 149L, 215L, 281L, 145L))
 })
